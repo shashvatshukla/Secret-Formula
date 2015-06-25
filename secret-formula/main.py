35f0f81b74858eee6add04228bcb5d55913d3dd7
wltan <dxgkrnl@gmail.com>
2015-06-26 00:06:56 +0800

    Added jinja2 functionality to aid the existing template system.
    Some modifications to app.yaml (adding some handler lines and jinja2
    library)
    - Though I have yet to figure out what the login: required does, no auto
    redirect if logged out or anything.
    
    Time spent: 2 hours + 1 hour from the previous commit
    
    AuthWebpage needs some refinement.

--- secret-formula/main.py
+++ secret-formula/main.py
@@ -1,23 +1,33 @@
 import webapp2
+import jinja2
+import os
+from google.appengine.api import users
+
+jjenv = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/res/webpage"))
 
 class Webpage(webapp2.RequestHandler):
-    def get(self):
-        with open('res/webpage/'+self.page, 'r') as f: 
-            for l in f:
-                self.response.write(l)
+    def get(self, args = {}):
+        tmp = jjenv.get_template(self.page)
+        self.response.write(tmp.render(args))
 
+class AuthWebpage(Webpage):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            super(AuthWebpage, self).get({'userid': user})
+        else:
+            self.redirect('../login')
 
 class Main(Webpage):
     page = 'Main.html'
     url = '/'
 
+class FormMgr(AuthWebpage):
+    page = 'FormsMgr.html'
+    url = '/forms'
 
-class Login(Webpage):
-    page = 'Login.html'
-    url = '/login'
 
-
-pagec = (Main, Login)
+pagec = (Main, FormMgr)
 pages = [(i.url, i) for i in pagec]
 
 
